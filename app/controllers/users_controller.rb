a434c8b62831df1a7758e20f8ed0d859cfba229a
Luong Viet Dung <luong.viet.dung@framgia.com>
2015-07-07 00:34:54 +0700

    Finish sign up user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save 
+      flash[:success] = "Wellcome to Sample App"
   		redirect_to @user
   	else
   		render 'new'
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
   end
   private
   	def user_params
-  		params.require(:user).permit(:name, :email, :password,:password_confirmation)
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   		
   	end
 end
