776571c3a0abe12ae5335562b34a29f08dc2d72c
Luong Viet Dung <luong.viet.dung@framgia.com>
2015-07-06 09:33:39 +0700

    Finish chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
   root 'static_pages#home'
 
   get 'help', to: 'static_pages#help'
   get 'about', to:'static_pages#about'
-  get 'static_pages/contact'
+  get 'contact', to: 'static_pages#contact'
+  get 'signup', to: 'user#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
