2347e4e93072d2fee9def883730a8f4394dc5b9c
Luong Viet Dung <luong.viet.dung@framgia.com>
2015-07-08 11:02:37 +0700

    Finish edit user and friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   		params[:session][:remember_me] == 1 ? remember(user) : forget(user)
   		log_in user
   		remember user
-  		redirect_to user
+  		redirect_to_back_or user 
   	else
   		flash[:danger] = "Invalid email / password combination"
   		render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,13 +26,23 @@ module SessionsHelper
 		user.remember
 		cookies.permanent.signed[:user_id] = user.id
 		cookies.permanent[:remember_token] = user.remember_token
+	end
 
+	def store_session
+		session[:forwarding_url] = request.url if request.get?
+	end
+	def redirect_to_back_or(default)
+		redirect_to (session[:forwarding_url] || default)
+		session.delete :forwarding_url
 	end
 
 	def forget(user)
 		user.forget
 		cookies.delete(:user_id)
 		cookies.delete(:remember_token)
+	end
+	def current_user?(user)
+		@current_user == user
 	end
 end
 
